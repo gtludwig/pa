067384a5def9d99836bb188ab705f9578eeba598
Gustavo T. Ludwig <gustavo.ludwig@gmail.com>
2019-05-07 15:45:49 +0100

    basic entity management

---
+++ core/src/main/java/ie/gtludwig/pa/model/BasePojo.java
@@ -0,0 +1,46 @@
+package ie.gtludwig.pa.model;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BasePojo extends _IPojo {
+    	private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "id", unique = true, insertable = true)
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
---
+++ core/src/main/java/ie/gtludwig/pa/model/_IPojo.java
@@ -0,0 +1,7 @@
+package ie.gtludwig.pa.model;
+
+import java.io.Serializable;
+
+public class _IPojo implements Serializable {
+}
+
